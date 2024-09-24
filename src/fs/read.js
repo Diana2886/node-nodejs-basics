4dcacfe87bc21982d06138363c319f01374c8642
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-24 22:52:48 +0500

    feat: add read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,24 @@
-const read = async () => {
-    // Write your code here 
-};
+import { access, constants, readFile } from 'node:fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
-await read();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const read = async () => {
+  const dir = join(__dirname, 'files')
+  const filePath = join(dir, 'fileToRead.txt')
+
+  access(filePath, constants.F_OK, (err) => {
+    if (err) {
+      throw new Error('FS operation failed')
+    } else {
+      readFile(filePath, 'utf-8', (err, data) => {
+        if (err) throw err
+        console.log(data)
+      })
+    }
+  })
+}
+
+await read()
