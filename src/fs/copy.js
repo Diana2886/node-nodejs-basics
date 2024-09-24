c01263cc2d95a8c179c31a2a20abdcd18f24b944
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-24 19:48:45 +0500

    feat: add create and copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,36 @@
-const copy = async () => {
-    // Write your code here 
-};
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import { access, constants, mkdir, readdir, copyFile } from 'fs'
 
-await copy();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const copy = async () => {
+  const dir = join(__dirname, 'files')
+  const targetDir = join(__dirname, 'files_copy')
+
+  readdir(dir, (err, files) => {
+    if (err) {
+      throw new Error('FS operation failed')
+    }
+
+    mkdir(targetDir, { recursive: false }, (err) => {
+      if (err) {
+        throw new Error('FS operation failed')
+      }
+    })
+
+    files.forEach((file) => {
+      const filePath = join(dir, file)
+      const targetFilePath = join(targetDir, file)
+
+      const callback = (err) => {
+        if (err) throw err
+      }
+
+      copyFile(filePath, targetFilePath, callback)
+    })
+  })
+}
+
+await copy()
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
-const create = async () => {
-    // Write your code here 
-};
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import { access, constants, writeFile } from 'fs'
 
-await create();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const create = async () => {
+  const dir = join(__dirname, 'files')
+  const filePath = join(dir, 'fresh.txt')
+  const content = 'I am fresh and young'
+
+  access(filePath, constants.F_OK, (err) => {
+    if (err) {
+      return
+    }
+    throw new Error('FS operation failed')
+  })
+
+  writeFile(filePath, content, (err) => {
+    if (err) {
+      throw err
+    }
+  })
+}
+
+await create()
