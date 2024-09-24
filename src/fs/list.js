5696bece68b6e4ce07045409eadbc5d16324d783
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-24 22:16:21 +0500

    feat: add list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
-const list = async () => {
-    // Write your code here 
-};
+import { access, readdir } from 'node:fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
-await list();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const list = async () => {
+  const dir = join(__dirname, 'files')
+
+  readdir(dir, (err, files) => {
+    if (err) {
+      throw new Error('FS operation failed')
+    }
+
+    console.log(files)
+  })
+}
+
+await list()
