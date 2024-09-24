0472f4461c1e84531c01fd087a4ad05162cdf1f4
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-24 20:30:22 +0500

    feat: add delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
-const remove = async () => {
-    // Write your code here 
-};
+import { access, constants, rm } from 'node:fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
-await remove();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const remove = async () => {
+  const dir = join(__dirname, 'files')
+  const filePath = join(dir, 'fileToRemove.txt')
+
+  access(filePath, constants.F_OK, (err) => {
+    if (err) {
+      throw new Error('FS operation failed')
+    } else {
+      rm(filePath, (err) => {
+        if (err) throw err
+      })
+    }
+  })
+}
+
+await remove()
