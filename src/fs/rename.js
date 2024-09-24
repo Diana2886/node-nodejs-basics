d414ee5dd3a63505aac27236d7391cf3b6ffbcff
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-24 20:14:40 +0500

    feat: add rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,29 @@
-const rename = async () => {
-    // Write your code here 
-};
+import fs, { access, constants } from 'node:fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
-await rename();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const rename = async () => {
+  const dir = join(__dirname, 'files')
+  const filePath = join(dir, 'wrongFilename.txt')
+  const targetFilePath = join(dir, 'properFilename.md')
+
+  access(filePath, constants.F_OK, (err) => {
+    if (err) {
+      throw new Error('FS operation failed')
+    }
+    access(targetFilePath, constants.F_OK, (err) => {
+      if (err) {
+        fs.rename(filePath, targetFilePath, (err) => {
+          if (err) throw err
+        })
+      } else {
+        throw new Error('FS operation failed')
+      }
+    })
+  })
+}
+
+await rename()
