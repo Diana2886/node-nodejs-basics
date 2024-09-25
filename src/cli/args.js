15a69224b078259e85c70316cf5e64b374877ed2
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-25 07:51:07 +0500

    feat: add args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
-const parseArgs = () => {
-    // Write your code here 
-};
+import { argv } from 'node:process'
 
-parseArgs();
+const parseArgs = () => {
+  const args = argv.slice(2)
+  const entries = []
+
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].slice(2)
+    const value = args[i + 1]
+
+    entries.push(`${propName} is ${value}`)
+  }
+
+  console.log(entries.join(', '))
+}
+
+parseArgs()
