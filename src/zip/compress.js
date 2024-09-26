1bb9769fdc8fd4afcea64fc83c553e8e673f41e0
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-26 08:46:50 +0500

    feat: add zip files

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,27 @@
-const compress = async () => {
-    // Write your code here 
-};
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import { createReadStream, createWriteStream } from 'fs'
+import { createGzip } from 'zlib'
+import { pipeline } from 'stream'
 
-await compress();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const compress = async () => {
+  const dir = join(__dirname, 'files')
+  const filePath = join(dir, 'fileToCompress.txt')
+  const targetFilePath = join(dir, 'archive.gz')
+
+  const readStream = createReadStream(filePath)
+  const writeStream = createWriteStream(targetFilePath)
+  const gzip = createGzip()
+
+  const pipelineStream = pipeline(readStream, gzip, writeStream, (err) => {
+    if (err) {
+      console.error('An error occurred:', err)
+      process.exitCode = 1
+    }
+  })
+}
+
+await compress()
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
-const decompress = async () => {
-    // Write your code here 
-};
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import { createReadStream, createWriteStream } from 'fs'
+import { createGunzip } from 'zlib'
+import { pipeline } from 'stream'
 
-await decompress();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const decompress = async () => {
+  const dir = join(__dirname, 'files')
+  const filePath = join(dir, 'archive.gz')
+  const targetFilePath = join(dir, 'fileToCompress.txt')
+
+  const readStream = createReadStream(filePath)
+  const writeStream = createWriteStream(targetFilePath)
+  const gunzip = createGunzip()
+
+  const pipelineStream = pipeline(readStream, gunzip, writeStream, (err) => {
+    if (err) {
+      console.error('An error occurred:', err)
+      process.exitCode = 1
+    }
+  })
+}
+
+await decompress()
