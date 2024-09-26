7f15eed91fbc1d5be9e66a4f9f6f7af0bc8091ed
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-26 07:28:18 +0500

    feat: add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import { createReadStream } from 'fs'
+import { createHash } from 'crypto'
+import { stdout } from 'process'
 
-await calculateHash();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const calculateHash = async () => {
+  const dir = join(__dirname, 'files')
+  const filePath = join(dir, 'fileToCalculateHashFor.txt')
+
+  const readStream = createReadStream(filePath)
+  const hash = createHash('sha256')
+
+  readStream.pipe(hash).setEncoding('hex').pipe(stdout)
+}
+
+await calculateHash()
