6719903e59e8f6f97e9ae5485647def5959096de
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-09-27 16:12:21 +0500

    feat: add wt files

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,18 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort } from 'worker_threads'
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
-};
+if (parentPort) {
+  const nthFibonacci = (n) =>
+    n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2)
 
-sendResult();
+  const sendResult = (n) => {
+    const result = nthFibonacci(n)
+
+    parentPort.postMessage(result)
+  }
+
+  parentPort.on('message', (n) => {
+    sendResult(n)
+  })
+} else {
+  console.error('This script should be run as a worker thread.')
+}
