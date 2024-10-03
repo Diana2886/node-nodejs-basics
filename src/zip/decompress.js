7f2e465138db262852707609d051c54714315964
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-10-03 22:29:42 +0500

    feat: change decompress.js using promises

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -2,26 +2,26 @@ import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 import { createReadStream, createWriteStream } from 'fs'
 import { createGunzip } from 'zlib'
-import { pipeline } from 'stream'
+import { pipeline } from 'stream/promises'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const decompress = async () => {
-  const dir = join(__dirname, 'files')
-  const filePath = join(dir, 'archive.gz')
-  const targetFilePath = join(dir, 'fileToCompress.txt')
+const dir = join(__dirname, 'files')
+const filePath = join(dir, 'archive.gz')
+const targetFilePath = join(dir, 'fileToCompress.txt')
 
+const decompress = async () => {
   const readStream = createReadStream(filePath)
   const writeStream = createWriteStream(targetFilePath)
   const gunzip = createGunzip()
 
-  const pipelineStream = pipeline(readStream, gunzip, writeStream, (err) => {
-    if (err) {
-      console.error('An error occurred:', err)
-      process.exitCode = 1
-    }
-  })
+  try {
+    const pipelineStream = await pipeline(readStream, gunzip, writeStream)
+  } catch (err) {
+    console.error('An error occurred:', err)
+    process.exitCode = 1
+  }
 }
 
 await decompress()
