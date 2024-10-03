e147bf8f39e97cbd489de47080f7c551e1b25423
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-10-03 21:34:39 +0500

    feat: change rename.js using promises

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,29 +1,32 @@
-import fs, { access, constants } from 'node:fs'
+import fs, { access, constants } from 'fs/promises'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const rename = async () => {
-  const dir = join(__dirname, 'files')
-  const filePath = join(dir, 'wrongFilename.txt')
-  const targetFilePath = join(dir, 'properFilename.md')
+const dir = join(__dirname, 'files')
+const filePath = join(dir, 'wrongFilename.txt')
+const targetFilePath = join(dir, 'properFilename.md')
+const errorMsg = 'FS operation failed'
 
-  access(filePath, constants.F_OK, (err) => {
-    if (err) {
-      throw new Error('FS operation failed')
-    }
-    access(targetFilePath, constants.F_OK, (err) => {
-      if (err) {
-        fs.rename(filePath, targetFilePath, (err) => {
-          if (err) throw err
-        })
+const rename = async () => {
+  try {
+    await access(filePath, constants.F_OK)
+
+    try {
+      await access(targetFilePath, constants.F_OK)
+      throw new Error(errorMsg)
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        await fs.rename(filePath, targetFilePath)
       } else {
-        throw new Error('FS operation failed')
+        throw err
       }
-    })
-  })
+    }
+  } catch (err) {
+    throw new Error(errorMsg)
+  }
 }
 
 await rename()
