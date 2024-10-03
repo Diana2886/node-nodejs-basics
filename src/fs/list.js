71309024801fe584d46cd54939ad91b14861576c
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-10-03 21:48:07 +0500

    feat: change list.js using promises

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,20 +1,20 @@
-import { access, readdir } from 'node:fs'
+import { access, readdir } from 'fs/promises'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
+const dir = join(__dirname, 'files')
+const errorMsg = 'FS operation failed'
+
 const list = async () => {
-  const dir = join(__dirname, 'files')
-
-  readdir(dir, (err, files) => {
-    if (err) {
-      throw new Error('FS operation failed')
-    }
-
+  try {
+    const files = await readdir(dir)
     console.log(files)
-  })
+  } catch (err) {
+    throw new Error(errorMsg)
+  }
 }
 
 await list()
