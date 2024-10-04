04b7de819b2babcd1b112bf33436c7b04cfbd757
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-10-04 08:22:51 +0500

    refactor: change calcHash.js by using pipeline

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -3,6 +3,8 @@ import { dirname, join } from 'path'
 import { createReadStream } from 'fs'
 import { createHash } from 'crypto'
 import { stdout } from 'process'
+import { pipeline } from 'stream/promises'
+import * as os from 'os'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -12,17 +14,14 @@ const calculateHash = async () => {
   const filePath = join(dir, 'fileToCalculateHashFor.txt')
 
   const readStream = createReadStream(filePath)
-  const hash = createHash('sha256')
+  const hash = createHash('sha256').setEncoding('hex')
 
-  readStream.on('error', (err) => {
-    console.error('Error reading file:', err.message)
-  })
-
-  readStream.pipe(hash).setEncoding('hex').pipe(stdout)
-
-  readStream.on('end', () => {
-    stdout.write('\n')
-  })
+  try {
+    await pipeline(readStream, hash, stdout, { end: false })
+    stdout.write(os.EOL)
+  } catch (err) {
+    console.error('Pipeline failed.', err.message)
+  }
 }
 
 await calculateHash()
