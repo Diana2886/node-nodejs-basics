a579eb6f193702576aec704f283520871d0b51f9
Diana Abdykassenova <diana_abdykassenova@epam.com>
2024-10-05 18:32:45 +0500

    feat: add possibility to copy nested folders

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
-import { access, constants, mkdir, readdir, copyFile } from 'fs/promises'
+import { access, constants, mkdir, readdir, copyFile, stat } from 'fs/promises'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -9,18 +9,38 @@ const dir = join(__dirname, 'files')
 const targetDir = join(__dirname, 'files_copy')
 const errorMsg = 'FS operation failed'
 
+const copyRecursive = async (src, dest) => {
+  const items = await readdir(src)
+
+  for (const item of items) {
+    const srcPath = join(src, item)
+    const destPath = join(dest, item)
+    const itemStat = await stat(srcPath)
+
+    if (itemStat.isDirectory()) {
+      await mkdir(destPath)
+      await copyRecursive(srcPath, destPath)
+    } else {
+      await copyFile(srcPath, destPath)
+    }
+  }
+}
+
 const copy = async () => {
   try {
-    const files = await readdir(dir)
+    await access(dir, constants.F_OK)
 
-    await mkdir(targetDir, { recursive: false })
+    try {
+      await access(targetDir, constants.F_OK)
+      throw new Error(errorMsg)
+    } catch (err) {
+      if (err.code !== 'ENOENT') {
+        throw new Error(errorMsg)
+      }
+    }
 
-    files.forEach(async (file) => {
-      const filePath = join(dir, file)
-      const targetFilePath = join(targetDir, file)
-
-      await copyFile(filePath, targetFilePath)
-    })
+    await mkdir(targetDir)
+    await copyRecursive(dir, targetDir)
   } catch (err) {
     throw new Error(errorMsg)
   }
